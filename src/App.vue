9dae45e3fbf580cee9287f92bc4163c759a96b90
rick999 <hpx210650@gmail.com>
2020-07-26 00:15:13 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <div class="top">
+      <v-container fill-height fluid>
+        <v-row>
+          <v-flex>
+            <v-col justify="center" align="center">
+              <v-avatar
+                class="profile"
+                color="grey"
+                size="125"
+                tile
+              >
+                <v-img :src="avatarImg"></v-img>
+              </v-avatar>
+              <v-list-item>
+                <v-list-item-content class="top-info">
+                  <v-list-item-title >侯柏超</v-list-item-title>
+                  <v-list-item-subtitle style="color:white;">
+                    國立雲林科技大學 資訊管理系 畢業
+                  </v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+            </v-col>
+          </v-flex>
+        </v-row>
+      </v-container>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      /* eslint-disable global-require */
+      avatarImg: require('@/assets/B10523004.jpg'),
+      /* eslint-enable global-require */
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+// <router-link to="/">Home</router-link>
+.top {
+  background-color: black;
+  height: 300px;
+  width: 100%;
+  color: white;
+}
+.top-info{
+  color: white;
+}
+#app {
+  font-family: "微軟正黑體", Helvetica, Arial, sans-serif;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="home">
+    <v-container fill-height fluid>
+      <v-row>
+        <v-flex>
+          <v-col justify="center" align="center">
+          <vue-typer
+          :text='["Hello 你好!","歡迎來到我的個人頁"]'
+          :repeat='Infinity'
+          :shuffle='false'
+          initial-action='typing'
+          :pre-type-delay='70'
+          :type-delay='70'
+          :pre-erase-delay='2000'
+          :erase-delay='250'
+          erase-style='select-all'
+          :erase-on-complete='false'
+          caret-animation='blink'
+          />
+          </v-col>
+        </v-flex>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue';
+// https://www.npmjs.com/package/vue-typer
+// https://cngu.github.io/vue-typer/#style-showcase
+import { VueTyper } from 'vue-typer';
+
+export default {
+  name: 'Home',
+  components: {
+    'vue-typer': VueTyper,
+  },
+};
+</script>
+<style lang="scss" scoped>
+.home {
+  margin-top: 100px;
+}
+.vue-typer {
+  font-size: 2.5em;
+}
+</style>
